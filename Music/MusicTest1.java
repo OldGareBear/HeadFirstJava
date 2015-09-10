4caceb65422b4e7cb6ba17c2ff0c35c9d5e70cef
Gary Bethea <betheagary@gmail.com>
2015-09-09 20:42:08 -0400

    add music test
    Signed-off-by: Gary Bethea <betheagary@gmail.com>

---
+++ Music/MusicTest1.java
@@ -0,0 +1,20 @@
+import javax.sound.midi.*;
+  
+public class MusicTest1 {
+  
+  public void play() {
+    try {
+      Sequencer sequencer = MidiSystem.getSequencer();
+    }
+    catch (MidiUnavailableException ex) {
+      System.out.println("Bummer.");
+    }
+    
+    System.out.println("We got the sequencer!");
+  }
+  
+  public static void main(String[] args) {
+    MusicTest1 mt = new MusicTest1();
+    mt.play();
+  }
+}
